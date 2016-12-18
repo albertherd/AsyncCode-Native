9d430d0e9c0b19b8cb28a418bc724e2d1ef08865
Albert Herd <albert.herd27@gmail.com>
2016-12-18 23:42:49 +0100

    updated with command line params

--- KSSAsyncNative/KSSAsyncNative.c
+++ KSSAsyncNative/KSSAsyncNative.c
@@ -6,11 +6,27 @@
 #include "threadpools.h"
 
 
-int main()
+int main(int argc, char** argv)
 {
-	BenchmarkThreads(FALSE);
-	BenchmarkThreads(TRUE);
-	BenchmarkThreadPools();
+	if (argc < 2) {
+		_tprintf(_TEXT("1) Benchmark Threads no Semaphore\r\n2) Benchmark Threads with Semaphore\r\n3) Benchmark ThreadPools"));
+		return 0;
+	}
+
+	int function = atoi(argv[1]);
+
+	switch (function) {
+	case 1:
+		BenchmarkThreads(FALSE);
+		break;
+	case 2:
+		BenchmarkThreads(TRUE);
+		break;
+	case 3: BenchmarkThreadPools();
+		break;
+	}
+
+
     return 0;
 }
 
